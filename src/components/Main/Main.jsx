fc7cc150648130caf1340e3bc77d4fad5f4a79dc
rejaul25 <rejaulislammr25@gmail.com>
2024-10-26 09:11:00 +0600

    small update

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,8 +11,8 @@ const Main = ({ toggleBtn, handleToggleBtn, handleSelectPlayers, selectPlayer, s
                 <div className='main_section_head md:flex  justify-between items-center px-4 xl:px-0'>
                     <h2 className='text-sm md:text-xl font-semibold'>{toggleBtn === "available" ? "Available Players" : <span className='md:text-2xl font-semibold'>Selected Player (<span>{selectPlayer.length}</span>/6)</span>}</h2>
                     <div className='avialable_and_selected_btn border-2 rounded-lg mt-3 md:mt-0'>
-                        <button onClick={() => { handleToggleBtn("available") }} className={`btn  w-[50%] md:w-auto text-xs md:text-lg ${toggleBtn === "available" ? 'bg-lime-400' : 'bg-none'} `}>Available</button>
-                        <button onClick={() => { handleToggleBtn("selected") }} className={`btn w-[50%] md:w-auto text-xs md:text-lg ${toggleBtn === "selected" ? 'bg-lime-400' : 'bg-none'} `}>Selected ( <span>{selectPlayer.length}</span> )</button>
+                        <button onClick={() => { handleToggleBtn("available") }} className={`btn  w-[50%] rounded-r-none hover:rounded-r-none md:w-auto text-xs md:text-lg ${toggleBtn === "available" ? 'bg-lime-400' : 'bg-none'} `}>Available</button>
+                        <button onClick={() => { handleToggleBtn("selected") }} className={`btn w-[50%] rounded-l-none hover:rounded-l-none md:w-auto text-xs md:text-lg ${toggleBtn === "selected" ? 'bg-lime-400' : 'bg-none'} `}>Selected ( <span>{selectPlayer.length}</span> )</button>
 
                     </div>
                 </div>
